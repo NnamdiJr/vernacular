dd2894fe880468c3716a0dbf9519de05a79ce6ab
Matt Bierner <mattbierner@gmail.com>
2016-05-16 22:51:16 -0700

    Fixed error for multi words with plurals

--- server/sub.js
+++ server/sub.js
@@ -5,53 +5,58 @@ const words = require('./words');
 const nounInflector = new natural.NounInflector();
 
 /**
- * 
+ * Expand a token into a set of zero or more choices to look for the trie.
  */
-const synonymResponse = (token, synonym) => {
-    const response = { token: token.token };
-    const id = token.id;
-    if (typeof id === 'number' || typeof id === 'string')
-        response.id = id;
-    if (synonym)
-        response.synonym = synonym;
-    return response;
+const expandToken = token => {
+    let c = words.normalizeWord(token.token);
+    if (!c.match(words.word_re_full))
+        return [];
+
+    const singular = nounInflector.singularize(c);
+    if (!singular || c === singular)
+        return [c];
+
+    return [c, singular];
 };
 
-
+/**
+ * Perform the actual substition.
+ */
 const sub = (trie, str) => {
     let open = [];
     let best = { list: null, weight: 0, i: 0 };
     let i = 0;
-    for (let token of str) {
-        let c = words.normalizeWord(token.token);
-        if (!c.match(words.word_re_full)) {
+    for (const token of str) {
+        const choices = expandToken(token);
+        if (!choices.length) {
             best.list = { value: { token: token.token, id: token.id }, rest: best.list };
             ++i;
             continue;
         }
-        
-        // Try additional normalization
-        if (!trie[c])
-            c = nounInflector.singularize(c);
-        
+
         // Try to add entries from dictionary for current key.
-        if (trie[c]) {
-            open.push({
-                list: best.list,
-                rest: trie,
-                weight: best.weight,
-                i: i
-            });
+        for (const c of choices) {
+            if (trie[c]) {
+                open.push({
+                    list: best.list,
+                    rest: trie,
+                    weight: best.weight,
+                    i: i
+                });
+            }
         }
-        
+
         best.list = { value: { token: token.token, id: token.id }, rest: best.list };
         ++best.weight;
 
         // Update existing working nodes
         const new_open = [];
-        for (let o of open) {
-            const next = o.rest[c];
-            if (next) {
+        for (const c of choices) {
+            for (const o of open) {
+                const next = o.rest[c];
+                if (!next)
+                    continue;
+
                 if (o.weight < best.weight && words.end in next) {
                     const tokens = str.slice(o.i, i + 1);
                     best = {
@@ -93,7 +98,6 @@ const sub = (trie, str) => {
 const thesurusizeTokens = module.exports.tokens = (trie, tokens) => {
     tokens = tokens
         .filter(x => x && typeof x.token === 'string' && x.token.length)
-
     return Promise.resolve(sub(trie, tokens));
 };
 
